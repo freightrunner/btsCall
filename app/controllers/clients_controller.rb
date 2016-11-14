a7de784b9946d7321a9a98189fe620a596c00685
Brian Arpaio <arpaio@gmail.com>
2016-11-14 19:06:05 +0000

    changed it so when displaying all clients on main page it will only show those with status type dnc

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,7 +6,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all.order('LOWER(name)')
+    @clients = Client.where(status: 'dnc').all.order('LOWER(name)')
     if params[:q]
       @clients = Client.where("name ILIKE ?", "%#{params[:q]}%").all.order('LOWER(name)')
     elsif params[:user_id]
