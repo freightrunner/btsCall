81fc65d186c15eaf51b9ab8df4bc7ba537bd5f91
Brian Arpaio <arpaio@gmail.com>
2016-11-16 19:34:04 +0000

    users#index will now be sorted alphabetically, added method to users controller to destroy and show users so the appropriate actions can be added to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,42 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user, only: [:show, :destroy] #might need to include :edit, :update
     before_action :check_admin, only: [:index]
     
     def index
-        @users = User.all
+        @users = User.all.order('LOWER(username)')
     end
     
+    def show
+    end
     
+#    def edit
+#    end
+    
+#    def update
+#    respond_to do |format|
+#      if @user.update(user_params)
+#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+#        format.json { render :show, status: :ok, location: @user }
+#      else
+#        format.html { render :edit }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
+#      end
+#    end
+#    end
+  
+    def destroy
+        @user.destroy
+        respond_to do |format|
+          format.html { redirect_to users_path, notice: 'User was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+    end
     
     private
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
     
     def check_admin
         @chkAcct = current_user
