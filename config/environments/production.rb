9a1a719d237506c8ec1fd913b8c38b4795845af2
Brian Arpaio <arpaio@gmail.com>
2016-11-18 21:08:02 +0000

    changed actual usernames and passwords to the variables in the doc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
     :address         => "smtp.sendgrid.net",
     :port            => "25",
     :authentication  => :plain,
-    :user_name       => ENV['app54945447@heroku.com'],
-    :password        => ENV['3yojjjmp3323'],
-    :domain          => ENV['sendgrid.com']
+    :user_name       => ENV['SENDGRID_USERNAME'],
+    :password        => ENV['SENDGRID_PASSWORD'],
+    :domain          => ENV['SENDGRID_DOMAIN']
   }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
