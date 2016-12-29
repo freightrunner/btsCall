437ed91db0c53e39c146cc2c920a42b19b1e970d
Brian Arpaio <arpaio@gmail.com>
2016-12-29 23:23:26 +0000

    changed something on a lark

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -14,7 +14,7 @@ app.Clients.prototype = {
             .autocomplete('instance')._renderItem = $.proxy(this._render, this);
     },
     
-    _render: function(ul, item) {
+    _renderItem: function(ul, item) {
         var markup = [
             '<span class="name"> + item.name + </span>',
             '<span class="address"> + item.address + </span>'
